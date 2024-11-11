a0d28f303029ff35bf22422e5f95348f659f5f8e
MarielaC <mchirinos@trafficlight.mx>
2024-11-11 16:23:05 -0600

    Inicio de sesion

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [usuario, setUsuario] = useState('marielac');
+  const [password, setPassword] = useState('1234');
+
+  const handleOK = async () => {
+    const data = { usuario: 'marielac', password: '1234' };
+
+    try {
+      // Realizar una solicitud POST a la URL de destino
+      const response = await fetch('http://centurion.key-city.com', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json', // Asegurarse de que estamos enviando JSON
+        },
+        body: JSON.stringify(data), // Convertir el objeto de datos a JSON
+      });
+
+      // Verificar si la solicitud fue exitosa
+      if (!response.ok) {
+        alert('Error en la solicitud');
+        return;
+      }
+
+      // Si la solicitud fue exitosa, redirigir a la URL
+      window.location.href = 'http://centurion.key-city.com';
+    } catch (error) {
+      console.error('Error en la solicitud:', error);
+      alert('Hubo un problema al enviar los datos');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label>Usuario</label>
+      <input
+        value={usuario}
+        onChange={(e) => setUsuario(e.target.value)}
+      />
+      <label>Contraseña</label>
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleOK}>Entrar</button>
     </div>
   );
 }
